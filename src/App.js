d70bafe754adf588a35d358d0ac3a8becebdc26a
Henri Landvik <henri.landvik@gmail.com>
2019-04-17 00:57:42 +0300

    Routing - Created routing for the site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Home from './pages/home'
+import Login from './pages/login'
+import Register from './pages/register'
+import NotFound from './pages/notfound'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-wrapper">
+			  	<Router>
+			    	<div className="Menu-wrapper">
+				      <ul>
+				        <li>
+				          <Link to="/">Home</Link>
+				        </li>
+				        <li>
+				          <Link to="/login">Login</Link>
+				        </li>
+				        <li>
+				          <Link to="/register">Register</Link>
+				        </li>				        
+				      </ul>	    	
+			    	</div>
+			    	<Switch>
+			    	<Route exact path="/" component={Home} />
+      				<Route path="/login" component={Login} />
+      				<Route path="/register" component={Register} />
+      				<Route component={NotFound} />
+      			</Switch>
+			  	</Router>
+        </header>    
       </div>
     );
   }
---
+++ src/pages/login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="Login">
+        <header className="Login-header">
+        </header>
+        <section className="Login-section">
+          <h1>Login</h1>
+          <p>This is the login page of the site.</p>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Login;
